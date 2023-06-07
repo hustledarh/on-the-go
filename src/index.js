e07ae4296d4de73535060a8b6c850f088ad0f6d9
Prateek Samaiya <samaiyaprateek94@gmail.com>
2023-06-07 08:36:08 +0530

    removed unnecesary imports

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavigationBar from './components/Navigation';
-import Footer from './components/Footer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

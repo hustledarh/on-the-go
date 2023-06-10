5ce177375af4da799d21c4524175a4ac4d4d4b02
Prateek Samaiya <samaiyaprateek94@gmail.com>
2023-06-10 07:16:39 +0530

    s

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavigationBar from "./components/Navigation";
 import UniqueLines from "./pages/UniqueLines";
 import About from "./pages/About";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
 function App() {
@@ -11,13 +11,10 @@ function App() {
     <Container className='px-0' fluid={true}>
       <NavigationBar/>
       <Container className="px-1 pb-5">
-        <UniqueLines/>
-      <BrowserRouter>
         <Routes>
           <Route path="/about" element={<About />} />
           <Route path="/" element={<UniqueLines />} />
         </Routes>
-      </BrowserRouter>
       </Container>
       <Footer/>
     </Container>

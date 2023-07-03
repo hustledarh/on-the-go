5eb30c8fd2b8f02f201b2a79ee04a43d1c050e87
Prateek Samaiya <samaiyaprateek94@gmail.com>
2023-07-03 07:42:55 +0530

    removed lines count

--- src/pages/JsonFormatter.js
+++ src/pages/JsonFormatter.js
@@ -6,8 +6,6 @@ import { useState } from "react";
 function JsonFormatter(){
 
     const [postContent, setPostContent] = useState('');
-    const [inputLinesCount, setInputLinesCount] = useState(0);
-    const [uniqueLinesCount, setUniqueLinesCount] = useState(0);
 
     function handleChange(event) {
         try {
@@ -22,15 +20,11 @@ return(
     <Container className=" h-100" fluid>
         <Row className="justify-content-around  h-100 py-1" >
         <Col className=" text-center  w-100 h-100 py-1"  >
-        <h1 className="">Input Lines</h1> 
         <textarea type="text" className="w-100 h-100 py-1" onChange={handleChange} placeholder="Input Lines"  />  
-        <p><b>input lines count</b> : {inputLinesCount}</p> 
         </Col>
 
         <Col className="text-center  w-100 h-100 py-1">
-        <h1>Unique Lines</h1>  
         <textarea className="w-100 h-100 py-1" value={postContent} type="text"  placeholder="Unique Lines" />    
-        <p><b>unique lines count</b> : {uniqueLinesCount}</p>
         </Col>
         </Row>
     </Container>

6a5e3d73c820d7ab5ebfca17a1c7a05cd9ed4eba
Prateek Samaiya <samaiyaprateek94@gmail.com>
2023-07-04 06:30:58 +0530

    corrected hint text for json textarea

--- src/pages/JsonFormatter.js
+++ src/pages/JsonFormatter.js
@@ -20,11 +20,11 @@ return(
     <Container className=" h-100" fluid>
         <Row className="justify-content-around  h-100 py-1" >
         <Col className=" text-center  w-100 h-100 py-1"  >
-        <textarea type="text" className="w-100 h-100 py-1" onChange={handleChange} placeholder="Input Lines"  />  
+        <textarea type="text" className="w-100 h-100 py-1" onChange={handleChange} placeholder="Input Json"  />  
         </Col>
 
         <Col className="text-center  w-100 h-100 py-1">
-        <textarea className="w-100 h-100 py-1" value={postContent} type="text"  placeholder="Unique Lines" />    
+        <textarea className="w-100 h-100 py-1" value={postContent} type="text"  placeholder="Formatted Json" />    
         </Col>
         </Row>
     </Container>
